c144a6f84cced30c3d5e3d1ef8c1ae2821abbe54
IGramajoO <igramajo@outlook.com>
2022-04-07 01:28:25 -0700

    initial implementation of springboot backend

---
+++ src/test/java/com/example/project_3_website/Project3WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_3_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
