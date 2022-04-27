5cee52706d42a36c2527c07c7c91ec13f46ab996
abeRoblesMartinez <aroblesmartinez@csumb.edu>
2022-04-27 12:21:09 -0700

    Added working rest controller

--- src/main/java/com/example/project_3_website/RestSpringBootController.java
+++ src/main/java/com/example/project_3_website/RestSpringBootController.java
@@ -1,9 +1,8 @@
 package com.example.project_3_website;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -13,12 +12,29 @@ import java.util.List;
 @RequestMapping(path="/heroApi")
 public class RestSpringBootController {
 
-    @GetMapping(value = "/getHeroesById")
-    public List<Object> getHeroes(@RequestParam int id){
-        String url = "https://superheroapi.com/api.php/109324175078057/"+id;
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] heroes = restTemplate.getForObject(url, Object[].class);
-        return Arrays.asList(heroes);
+    @GetMapping
+    public ResponseEntity<?> getHero() {
+        try {
+            HttpRequest request = HttpRequest
+                    .get("https://www.superheroapi.com/api.php/109324175078057")
+                    .connectTimeout(120000);
+            String res = request.body();
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
+    @GetMapping("/restHero/{id}")
+    public ResponseEntity<?> getHeroes(@PathVariable String id) {
+        try {
+            String uri="https://www.superheroapi.com/api.php/109324175078057/"+id+"/powerstats";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(uri, String.class);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("Error!, Please try again", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
