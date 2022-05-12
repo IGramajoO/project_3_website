9d1e855cd969ae61bb9ba86728472affedc36a42
LuiM112 <89751770+LuiM112@users.noreply.github.com>
2022-05-11 21:01:41 -0700

    Permitted more areas that won't require google sign in

--- src/main/java/com/example/project_3_website/SecurityConfig.java
+++ src/main/java/com/example/project_3_website/SecurityConfig.java
@@ -10,7 +10,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.antMatcher("/**").authorizeRequests()
-                .antMatchers("/", "/login.html", "/index.html","/login").permitAll()
+                .antMatchers("/", "/login.html", "/index.html","/login", "/accountPage.html", "/createAccount",
+                        "/createAccount.html")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
