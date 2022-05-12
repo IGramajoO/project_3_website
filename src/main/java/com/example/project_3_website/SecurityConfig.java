80b753e221553654e3ee577c0c2afb5a30dec086
LuiM112 <89751770+LuiM112@users.noreply.github.com>
2022-05-11 22:06:11 -0700

    Work in progress to solve the force google login issue

--- src/main/java/com/example/project_3_website/SecurityConfig.java
+++ src/main/java/com/example/project_3_website/SecurityConfig.java
@@ -1,21 +1,46 @@
 package com.example.project_3_website;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.antMatcher("/**").authorizeRequests()
-                .antMatchers("/", "/login.html", "/index.html","/login", "/accountPage.html", "/createAccount",
-                        "/createAccount.html","/fragments.html","/team.html")
+        http.cors(config-> config.configurationSource(corsConfigurationSource())).authorizeRequests()
+                .antMatchers("/", "/login.html", "/index.html","/login/**", "/accountPage.html", "/createAccount",
+                        "/createAccount.html","/fragments.html","/team.html","/accountPage/**", "/style.css",
+                        "/script.js","/team")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+        corsConfiguration.addAllowedHeader("X-Requested-With");
+        corsConfiguration.addAllowedHeader("Content-Type");
+        corsConfiguration.addAllowedHeader("Accept");
+        corsConfiguration.addAllowedHeader("Origin");
+        corsConfiguration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 
 }
